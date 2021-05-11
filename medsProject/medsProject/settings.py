b5753e3daeebd96f7c811be242ffc64a10a75c68
Antoni Maciag <antoni.maciag1@gmail.com>
2021-05-11 21:59:21 +0200

    security xd

--- medsProject/medsProject/settings.py
+++ medsProject/medsProject/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a7ip8hal0q)uw(ej*rchybeawl5j#)x+sni$a%+ozlh4ur#*(7'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
