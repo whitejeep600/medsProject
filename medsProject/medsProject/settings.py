69e364277d7ec9dd709e055fced0a3409a63e890
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2021-05-13 19:08:51 +0200

    Added django_tables2 as dependency

--- medsProject/medsProject/settings.py
+++ medsProject/medsProject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_tables2"
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.request"
             ],
         },
     },
