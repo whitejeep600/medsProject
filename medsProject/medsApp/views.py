20cc6f8332f8a4bf8cb3a120e4ea2e6a61940b16
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2021-05-14 00:13:55 +0200

    Added django filters

--- medsProject/medsApp/views.py
+++ medsProject/medsApp/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import *
 from .table import *
+import django_filters.filterset as filterset
 
 def index(request):
 
@@ -13,16 +14,9 @@ def index(request):
     if sortBy:
         drugs = drugs.order_by(sortBy)
 
-    filterSlug = request.GET.get("filter")
-    filters = None
-    if filterSlug:
-        filters = dict([singleFilter.split(":",1) for singleFilter in filterSlug.split(",")])
-        # also allow matches which contain the searched keyword
-        lazyFilters = {(key+"__contains"):value for (key,value) in filters.items()}
-        drugs = drugs.filter(**lazyFilters)
+    filter = DrugFilterSet(request.GET, queryset=drugs)
 
-    drugTable = DrugTable(drugs).paginate(page=request.GET.get("page",1),per_page=25)
-
+    drugTable = DrugTable(filter.qs).paginate(page=request.GET.get("page",1),per_page=25)
     template = loader.get_template('medsApp/mainpage.html')
 
     def pack(*names, locals=None):
@@ -30,7 +24,7 @@ def index(request):
             locals = {}
         return {key: locals.get(key, globals().get(key)) for key in names}
 
-    context = pack("drugTable",locals=locals())
+    context = pack("drugTable","filter",locals=locals())
     return HttpResponse(template.render(context, request))
 
 # Create your views here.
