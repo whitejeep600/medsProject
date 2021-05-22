dd461a505433c43cb10cb055ea965cab027123ff
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2021-05-22 17:57:00 +0200

    Added pipe to the middle of the cells

--- medsProject/medsApp/table.py
+++ medsProject/medsApp/table.py
@@ -9,19 +9,19 @@ def key_cols(s):
     return s
 
 class DrugTable(tables.Table):
-    diff_date = tables.Column(accessor='diff_date', verbose_name='Date')
-    diff_active_substance = tables.Column(accessor='diff_active_substance', verbose_name='Active substance')
-    diff_payment_lvl = tables.Column(accessor='diff_payment_lvl', verbose_name='Payement level')
-    diff_official_price = tables.Column(accessor='diff_official_price', verbose_name='Official price')
-    diff_limit_group = tables.Column(accessor='diff_limit_group', verbose_name='Limit group')
-    diff_med_form = tables.Column(accessor='diff_med_form', verbose_name='Medicine form')
-    diff_dose = tables.Column(accessor='diff_dose', verbose_name='Dose')
-    diff_med_name = tables.Column(accessor='diff_med_name', verbose_name='Medicine name')
-    diff_retail_price = tables.Column(accessor='diff_retail_price', verbose_name='Retail price')
-    diff_pack_size = tables.Column(accessor='diff_pack_size', verbose_name='Pack size')
-    diff_refund_limit = tables.Column(accessor='diff_refund_limit', verbose_name='Refund limit')
-    diff_patient_payment = tables.Column(accessor='diff_patient_payment', verbose_name='Patient payment')
-    diff_wholesale_price = tables.Column(accessor='diff_wholesale_price', verbose_name='Wholesale price')
+    diff_date = tables.TemplateColumn("{{ record.diff_date | linebreaksbr }}",verbose_name='Date')
+    diff_active_substance = tables.TemplateColumn("{{record.diff_active_substance|linebreaksbr}}" , verbose_name='Active substance')
+    diff_payment_lvl = tables.TemplateColumn("{{record.diff_payment_lvl|linebreaksbr}}" , verbose_name='Payement level')
+    diff_official_price = tables.TemplateColumn("{{record.diff_official_price|linebreaksbr}}" , verbose_name='Official price')
+    diff_limit_group = tables.TemplateColumn("{{record.diff_limit_group|linebreaksbr}}" , verbose_name='Limit group')
+    diff_med_form = tables.TemplateColumn("{{record.diff_med_form|linebreaksbr}}" , verbose_name='Medicine form')
+    diff_dose = tables.TemplateColumn("{{record.diff_dose|linebreaksbr}}" , verbose_name='Dose')
+    diff_med_name = tables.TemplateColumn("{{record.diff_med_name|linebreaksbr}}" , verbose_name='Medicine name')
+    diff_retail_price = tables.TemplateColumn("{{record.diff_retail_price|linebreaksbr}}" , verbose_name='Retail price')
+    diff_pack_size = tables.TemplateColumn("{{record.diff_pack_size|linebreaksbr}}" , verbose_name='Pack size')
+    diff_refund_limit = tables.TemplateColumn("{{record.diff_refund_limit|linebreaksbr}}" , verbose_name='Refund limit')
+    diff_patient_payment = tables.TemplateColumn("{{record.diff_patient_payment|linebreaksbr}}" , verbose_name='Patient payment')
+    diff_wholesale_price = tables.TemplateColumn("{{record.diff_wholesale_price|linebreaksbr}}" , verbose_name='Wholesale price')
     class Meta:
         model = Drug
         attrs = {'class': 'table'}
